85685addbbe6b64465a1f36fbe1c9daf5b7152ea
Raghu Betina <raghu@starterleague.com>
2012-11-04 15:55:52 -0600

    Fixed seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Director.destroy_all
+Actor.destroy_all
 Movie.destroy_all
 Role.destroy_all
 
 directors = [
-  { :name => "Christopher Nolan", :dob => "07/30/1970" },
-  { :name => "Francis Ford Coppola", :dob => "04/07/1939" },
-  { :name => "Frank Darabont", :dob => "01/29/1959" },
-  { :name => "Ron Howard", :dob => "03/01/1954" },
-  { :name => "George Lucas", :dob => "05/14/1944" },
-  { :name => "Steven Spielberg", :dob => "12/18/1946" }
+  { :name => "Christopher Nolan", :dob => Date.parse("30/07/1970") },
+  { :name => "Francis Ford Coppola", :dob => Date.parse("07/04/1939") },
+  { :name => "Frank Darabont", :dob => Date.parse("28/01/1959") },
+  { :name => "Ron Howard", :dob => Date.parse("01/03/1954") },
+  { :name => "George Lucas", :dob => Date.parse("14/05/1944") },
+  { :name => "Steven Spielberg", :dob => Date.parse("18/12/1946") }
+]
+
+actors = [
+  { :name => "Christian Bale", :dob => Date.parse("30/01/1974") },
+  { :name => "Al Pacino", :dob => Date.parse("25/04/1940") },
+  { :name => "Tim Robbins", :dob => Date.parse("16/10/1958") },
+  { :name => "Harrison Ford", :dob => Date.parse("13/07/1942") },
+  { :name => "Morgan Freeman", :dob => Date.parse("06/01/1937") }
 ]
 
 movies = [
@@ -32,13 +41,18 @@ movies = [
 ]
 
 roles = [
-  { :name => "Bruce Wayne", :movie => "The Dark Knight" },
-  { :name => "Michael Corleone", :movie => "The Godfather" },
-  { :name => "Andy Dufresne", :movie => "The Shawshank Redemption" },
-  { :name => "Red Redding", :movie => "The Shawshank Redemption" }
+  { :name => "Bruce Wayne", :movie => "The Dark Knight", :actor => "Christian Bale" },
+  { :name => "Michael Corleone", :movie => "The Godfather", :actor => "Al Pacino" },
+  { :name => "Indiana Jones", :movie => "Raiders of the Lost Ark", :actor => "Harrison Ford" },
+  { :name => "Han Solo", :movie => "Star Wars: A New Hope", :actor => "Harrison Ford" },
+  { :name => "Andy Dufresne", :movie => "The Shawshank Redemption", :actor => "Tim Robbins" },
+  { :name => "Red Redding", :movie => "The Shawshank Redemption", :actor => "Morgan Freeman" }
 ]
 
+
+
 Director.create directors
+Actor.create actors
 
 movies.each do |movie|
   d = Director.find_by_name(movie[:director])
@@ -47,8 +61,17 @@ end
 
 roles.each do |role|
   m = Movie.find_by_title(role[:movie])
-  Role.create :name => role[:name], :movie_id => m.id
+  a = Actor.find_by_name(role[:actor])
+  Role.create :name => role[:name], :movie_id => m.id, :actor_id => a.id
 end
+
+
+
+
+
+
+
+
 
 
 
