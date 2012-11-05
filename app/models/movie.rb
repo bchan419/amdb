1af672b6997f7b2d444a19f3c8ddb57caf6a2e00
Raghu Betina <raghu@starterleague.com>
2012-11-05 10:52:22 -0600

    Added Cast and Filmography.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ActiveRecord::Base
   attr_accessible :director_id, :title, :year, :director
   
   belongs_to :director
+  has_many :roles
+  has_many :actors, :through => :roles
   
   # def director
   #   Director.find_by_id(self.director_id)
