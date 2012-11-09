dcee683ac262744c7c66811e71e9d3837dec70d7
Raghu Betina <raghu@starterleague.com>
2012-11-09 10:43:52 -0600

    Ready to secure voting.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def introduce
     user = User.find_by_email(params[:email])
     
-    if user != nil
+    if user != nil && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Welcome back, #{user.name}!"
     else
